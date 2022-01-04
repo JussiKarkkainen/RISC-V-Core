f4d296c762ae5ea8f544334a5814f53581bfffcf
JussiKarkkainen <juskar@gmail.com>
2022-01-04 14:24:05 +0200

    rewrite regfile

--- regfile.v
+++ regfile.v
@@ -1,40 +1,30 @@
 module regfile
-  (input clock,
+  (input clk,
   input [4:0] read_a,
   input [4:0] read_b,
   input [4:0] write_idx,
   input [31:0] data,
   input write_enable,
-  output reg [31:0] a,
-  output reg [31:0] b
+  output [31:0] a,
+  output [31:0] b
   );
 
 
 
 reg [31:0] regs[0:31];
 
-always @(posedge clock)
+assign a = regs[read_a];
+assign b = regs[read_b];
+
+always @(negedge clk)
   begin
     if (write_enable)
       begin
         if (write_idx != 0)
-          begin
-            regs[write_idx] <= data;
-          end
+          regs[write_idx] <= data;
       end
   end
 
-always @ (*)
-  begin
-    if (read_a == 5'd0)
-      a <= 32'b0;
-    else
-      a <= regs[read_a];
-    if (read_b == 5'd0)
-      b <= 32'b0;
-    else
-      b <= regs[read_b];
-  end
 
         
 endmodule
