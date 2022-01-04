8952f39b843205beda8263cd3bec18ebcdffe9db
JussiKarkkainen <juskar@gmail.com>
2022-01-04 15:28:47 +0200

    rewrite ram

--- ram.v
+++ ram.v
@@ -1,46 +1,35 @@
 module ram (
   input clk,
 
-  // write enable
   input w_enable,
-  
-  // instruction fetch address, output
-  input [31:0] i_addr,
+   
+  input [13:0] i_addr,
   output reg [31:0] i_data,
   
-  // data in, data fetch address, output
+  input [2:0] d_size,
   input [31:0] d_in,
-  input [31:0] d_addr,
+  input [13:0] d_addr,
   output reg [31:0] d_out_data
   );
 
-// 16 kb memory
+
 reg [31:0] mem[0:4095];
 
-// read memory
-always @ *
-  begin
-    i_data <= mem[i_addr];
-    d_out_data <= mem[d_addr];
-  end
-
-// write to memory
 always @(posedge clk)
   begin
+    // read
+    i_data <= mem[i_addr[13:2]];
+    d_out_data <= mem[d_addr[13:2]];
+
+    // write, support different size stores
     if (w_enable == 1'b1)
       begin
-        mem[d_addr] <= d_in;
+        case (d_size)
+          3'b000: mem[d_addr[13:2]] <= d_in[7:0];
+          3'b001: mem[d_addr[13:2]] <= d_in[15:0];
+          3'b010: mem[d_addr[13:2]] <= d_in;
+        endcase
       end
   end
-
-/*
-    else
-      begin
-        i_data <= mem[i_addr];
-        d_out <= mem[d_addr]; 
-      end
-  end
-*/
-
 
 endmodule
