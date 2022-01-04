3346626c99c18e37a035d9a504604f1f6480d8a4
JussiKarkkainen <juskar@gmail.com>
2022-01-04 19:12:40 +0200

    new testbench

--- core_tb.sv
+++ core_tb.sv
@@ -1,162 +1,55 @@
-`timescale 1 ns/10 ps
 `include "core.v"
 
-module core_tb;
-
-
+module testbench;
 
   reg clk;
-  reg reset;
+  reg resetn;
+  reg [7:0] cnt;
   wire [31:0] pc;
-
-
-  core dut (
-    .clk(clk),
-    .reset_n(reset),
-    .pc(pc)
-    );
-
-
-
-
-  initial 
-    begin
-      clk = 0;
-      reset = 1;
-
-      $display("loading memory");
-      $readmemh("mem.txt", dut.ram.mem);
-      $display("first mem: %h", dut.ram.mem[1]);
-      $display("memory loaded");
-      reset = 0;
-    end
-
-
-  always 
-    #50 clk <= !clk;
   
+  core dut (
+    .clk (clk),
+    .reset_n (resetn),
+    .pc(pc)
+  );
+
+  initial begin
+    clk = 0;
+    cnt = 0;
+    resetn = 0;
+
+    //$readmemh("test-cache/rv32ui-p-add", c.ram.mem);
+    $readmemh("test-cache/rv32ui-p-add", dut.ram.mem);
+    $display("starting test", cnt);
+    @(posedge clk);
+    resetn = 1;
+  end
+
+  always
+    #5 clk = !clk;
 
   always @(posedge clk) begin
-    $display("pc: %h\n", 
-              "reset: %b\n", 
-              "Opcode: %b\n", 
-              "funct3: %b\n", 
-              "funct7: %b\n", 
-              "alu_x: %h\n",
-              "alu_y: %h\n", 
-              "csr_we: %b\n", 
-              "csr_re: %b\n", 
-              "csr_funct3: %b\n", 
-              "csr_addr: %h\n", 
-              "csr_data_i: %h\n",
-              "ram_w_enable: %b\n", 
-              "ram_d_addr: %h\n", 
-              "reg_w_enable: %b\n", 
-              "reg_write_idx: %h\n",
-              dut.pc, 
-              dut.reset_n, 
-              dut.opcode, 
-              dut.funct3, 
-              dut.funct7, 
-              dut.alu_x, 
-              dut.alu_y, 
-              dut.csr_we, 
-              dut.csr_re, 
-              dut.csr_funct3, 
-              dut.csr_addr, 
-              dut.csr_data_i,
-              dut.ram_w_enable, 
-              dut.ram_d_addr, 
-              dut.reg_w_enable, 
-              dut.reg_write_idx);
-   $finish; 
+    cnt <= cnt + 1;
+    resetn <= 0;
   end
 
-  integer i;
-  initial begin
-    for (i=0; i<32; i=i+1) begin
-      $display("%d:  %h", i, dut.regfile.regs[i]);
-    end
+
+
+  always @(posedge clk) begin
+    if (dut.step[6] == 1'b1) begin
+   /* 
+    $display("ins %h pc %h opcode %b funct3 %b funct7 %b ram_w_enable %b reg_w_enable %b rs1 %b rs2 %b imm_i %h imm_s %h imm_b %h imm_u %h imm_j %h rd %b alu_x %b alu_y %b", 
+      c.ram_i_data, c.pc, c.opcode, c.alu_funct3, c.alu_funct7, c.ram_w_enable, c.reg_w_enable, c.reg_read_a, c.reg_read_b, c.imm_i, c.imm_s, c.imm_b, c.imm_u, c.imm_j, c.rd, c.alu_x, c.alu_y);
+      */
+    $display("ram_i_data: %h reset: %b pc: %h opcode:%b funct3:%b alu_x:%h alu_y:%h alu_out:%h ram_d_addr:%h ram_d_data:%h",
+      dut.ram_i_data, dut.reset_n, dut.pc, dut.opcode, dut.alu_funct3, dut.alu_x, dut.alu_y, dut.alu_out, dut.ram_d_addr, dut.ram_d_out);
     
+    end
+  end
+  
+  initial begin
+    #700
+    $display("Finished ", cnt);
     $finish;
   end
-
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    $display("x1:  %h\n", 
-             "x2:  %h\n", 
-             "x3:  %h\n", 
-             "x4:  %h\n",
-             "x5:  %h\n", 
-             "x6:  %h\n", 
-             "x7:  %h\n", 
-             "x8:  %h\n", 
-             "x9:  %h\n", 
-             "x10:  %h\n", 
-             "x11:  %h\n", 
-             "x12:  %h\n", 
-             "x13:  %h\n", 
-             "x14:  %h\n", 
-             "x15:  %h\n", 
-             "x16:  %h\n", 
-             "x17:  %h\n", 
-             "x18:  %h\n", 
-             "x19:  %h\n",
-             "x20:  %h\n", 
-             "x21:  %h\n", 
-             "x22:  %h\n", 
-             "x23:  %h\n", 
-             "x24:  %h\n", 
-             "x25:  %h\n", 
-             "x26:  %h\n",
-             "x28:  %h\n", 
-             "x31:  %h\n", 
-             "x32:  %h\n",
-             dut.regfile.regs[0],
-             dut.regfile.regs[1],
-             dut.regfile.regs[2],
-             dut.regfile.regs[3],
-             dut.regfile.regs[4],
-             dut.regfile.regs[5],
-             dut.regfile.regs[6],
-             dut.regfile.regs[7],
-             dut.regfile.regs[8],
-             dut.regfile.regs[9],
-             dut.regfile.regs[10],
-             dut.regfile.regs[11],
-             dut.regfile.regs[12],
-             dut.regfile.regs[13],
-             dut.regfile.regs[14],
-             dut.regfile.regs[15],
-             dut.regfile.regs[16],
-             dut.regfile.regs[17],
-             dut.regfile.regs[18],
-             dut.regfile.regs[19],
-             dut.regfile.regs[20],
-             dut.regfile.regs[21],
-             dut.regfile.regs[22],
-             dut.regfile.regs[23],
-             dut.regfile.regs[24],
-             dut.regfile.regs[25],
-             dut.regfile.regs[26],
-             dut.regfile.regs[27],
-             dut.regfile.regs[28],
-             dut.regfile.regs[29],
-             dut.regfile.regs[30],
-             dut.regfile.regs[31]
-             );
-             */
